edd9dc86a350213d7057dd77c91ff16a028b7b38
Rodrigo Raw <rodrigo.rraw@gmail.com>
2026-03-30 23:18:03 -0300

    Primeira versão da Loja A Doninha Encantada

---
+++ Project - A Doninha Encantada/Program.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;  
+using System.Threading.Tasks;
+using Project_A_Doninha_Encantada_Classes;
+
+namespace A_Doninha_Encantada_Painel
+{ 
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Instância da classe "ProdutoRPG" para armazenar os produtos do estoque
+            List<ProdutoRPG> produtoEstoque = new List<ProdutoRPG>(); 
+            List<Venda> vendasRealizadas = new List<Venda>();
+
+
+            // --- Itens para testar na loja, se quiser usar, selecionar todos com o mouse e apertar (Ctrl + ;) ---
+
+            // --- EQUIPAMENTOS ---
+            produtoEstoque.Add(new Equipamento("EQ01", "Espada Longa de Ferro", 150.00m, 75, "Arma Corpo-a-Corpo"));
+            produtoEstoque.Add(new Equipamento("EQ02", "Escudo de Carvalho Anfíbio", 45.00m, 112, "Defesa Mão Secundária"));
+            produtoEstoque.Add(new Equipamento("EQ03", "Armadura Escamas de Dragão", 1250.00m, 92, "Armadura Pesada"));
+            produtoEstoque.Add(new Equipamento("EQ04", "Cajado da Estrela Cadente", 320.50m, 81, "Arma Mágica"));
+            produtoEstoque.Add(new Equipamento("EQ05", "Botas Aladas de Hermes", 150.00m, 0, "Acessório Mágico"));
+
+            // --- CONSUMÍVEIS ---
+            produtoEstoque.Add(new Consumivel("CS01", "Poção de Cura Menor", 25.50m, 30, "Cura 50 HP"));
+            produtoEstoque.Add(new Consumivel("CS02", "Elixir da Invisibilidade", 200.00m, 30, "Invisibilidade por 3 turnos"));
+            produtoEstoque.Add(new Consumivel("CS03", "Pergaminho: Bola de Fogo", 120.00m, 50, "Dano em Área (Fogo)"));
+            produtoEstoque.Add(new Consumivel("CS04", "Ração de Viagem Elfica", 5.50m, 50, "Cura 10 HP e remove Fome"));
+            produtoEstoque.Add(new Consumivel("CS05", "Lágrima de Fênix", 5000.00m, 10, "Ressurreição Completa"));
+
+            int opcoes;
+            do
+            {
+                // Painel de opções para o usuário escolher entre as funcionalidades do sistema
+                Console.Clear();
+                Console.WriteLine("====================================");  
+                Console.WriteLine("       A Doninha Encantada RPG        ");
+                Console.WriteLine("====================================");
+                Console.WriteLine("Bem-vindo à Doninha Encantada!");
+                Console.WriteLine("1. Gestão de Estoque");
+                Console.WriteLine("2. Controle de Vendas");
+                Console.WriteLine("3. Relatórios");
+                Console.WriteLine("4. Sair");
+                Console.Write("\nEscolha uma opção: ");
+
+                if (!int.TryParse(Console.ReadLine(), out opcoes))
+                {
+                    opcoes = 0; // Força cair no default do switch para opções inválidas
+                }
+
+                switch (opcoes)
+                {
+                    case 1:   // Gestão de Estoque (cadastro de itens, atualização de preços, e reposição de estoque)  
+                        Console.Clear();
+                        Console.WriteLine("--- Gestão de Estoque ---");
+                        Console.WriteLine("1. Cadastrar Novo Equipamento");
+                        Console.WriteLine("2. Cadastrar Novo Consumível");
+                        Console.WriteLine("3. Repor Estoque de item existente");
+                        Console.WriteLine("4. Atualizar preço de item existente");
+                        Console.Write("\nEscolha: ");
+
+                        string escolha = Console.ReadLine();
+
+                        // Lógica para cadastro de novos itens ou reposição de estoque
+                        try
+                        {
+                            if (escolha == "1" || escolha == "2") // Cadastro de novos itens (equipamentos ou consumíveis)
+                            {
+                                Console.Clear();
+                                Console.Write("Digite um Id para o item: "); string id = Console.ReadLine();
+                                Console.Write("Digite um nome para o item: "); string nome = Console.ReadLine();
+                                Console.Write("Defina o preço do item: "); decimal preco = decimal.Parse(Console.ReadLine());
+                                Console.Write("Defina uma quantidade inicial para o item: "); int quantidade = int.Parse(Console.ReadLine());
+
+                                if (escolha == "1") // Cadastro de Equipamento  
+                                {
+                                    Console.Write("Escolha qual será o tipo de equipamento: "); string tipoEquipamento = Console.ReadLine();
+                                    produtoEstoque.Add(new Equipamento(id, nome, preco, quantidade, tipoEquipamento));
+                                    Console.WriteLine("\nEquipamento cadastrado com sucesso!");
+                                }
+                                else // Cadastro de Consumível
+                                {
+                                    Console.Write("Escolha qual será o efeito do consumível: "); string efeito = Console.ReadLine();
+                                    produtoEstoque.Add(new Consumivel(id, nome, preco, quantidade, efeito));
+                                    Console.WriteLine("\nConsumível cadastrado com sucesso!");
+                                }
+                            }
+                            else if (escolha == "3") // Reposição de estoque para um item existente
+                            {
+                                Console.Clear();
+                                Console.Write("Digite o ID do item que deseja repor: ");
+                                string idRepor = Console.ReadLine();
+
+                                ProdutoRPG produtoRepor = produtoEstoque.Find(p => p.Id == idRepor);
+
+                                if (produtoRepor != null)
+                                {
+                                    Console.Write("Digite a quantidade a ser reposta: "); int quantidadeRepor = int.Parse(Console.ReadLine());
+                                    produtoRepor.ReporEstoque(quantidadeRepor);
+                                    Console.WriteLine("\nEstoque atualizado com sucesso!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Produto não encontrado no estoque...");
+                                }
+                            }
+                            else if (escolha == "4") // Atualização de preço para um item existente
+                            {
+                                Console.Clear();
+                                Console.Write("Digite o ID do item que deseja atualizar o preço: ");
+                                string idAtualizar = Console.ReadLine();
+                                ProdutoRPG produtoAtualizar = produtoEstoque.Find(p => p.Id == idAtualizar);
+
+                                if (produtoAtualizar != null)
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine($"Preço atual do item {produtoAtualizar.Nome}: {produtoAtualizar.Preco:C}");
+                                    Console.Write("Digite o novo preço do item: "); decimal novoPreco = decimal.Parse(Console.ReadLine());
+                                    produtoAtualizar.AtualizarPreco(novoPreco);
+                                    Console.WriteLine("\nPreço atualizado com sucesso!");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Produto não encontrado no estoque...");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Opção inválida. Retornando ao menu principal...");
+                            }
+
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine($"Ocorreu um erro: {ex.Message}. Operação cancelada.");
+                        }
+                        Console.WriteLine("\n------------------------------------");
+                        Console.WriteLine("Pressione qualquer tecla outra tecla para voltar...");
+                        Console.ReadKey(); 
+                        break;
+
+                    case 2:  // Controle de Vendas (venda de itens, cálculo de totais, e registro de vendas)
+                        Console.Clear();
+                        Console.WriteLine("--- Controle de Vendas ---");
+                        Console.WriteLine("Tipo de itens para venda:");
+                        Console.WriteLine("-Equipamentos ID(EQ00)-");
+                        Console.WriteLine("-Consumíveis ID(CS00)-");
+                        Console.Write("\nDigite o ID do item que deseja vender: ");
+
+                        // Lógica para buscar o produto no estoque usando "LINQ"
+                        string idVenda = Console.ReadLine();
+                        ProdutoRPG produtoVenda = produtoEstoque.Find(p => p.Id == idVenda);
+
+                        
+                        if(produtoVenda != null) // Produto encontrado no estoque, prosseguir com a venda
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"Produto encontrado: {produtoVenda.Nome} | Estoque Atual: {produtoVenda.Quantidade} | Preço: {produtoVenda.Preco:C}");
+
+                            if (produtoVenda.Quantidade <= 0) // Verifica se o produto está esgotado
+                            {
+                                Console.WriteLine("Produto esgotado. Não é possível realizar a venda.");
+                                Console.WriteLine("\n------------------------------------");
+                                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                                Console.ReadKey();
+                                break; // Sai do case para retornar ao menu principal
+                            }
+                            Console.Write("Quantidade a vender: ");
+
+                            if (int.TryParse(Console.ReadLine(), out int quantidadeVenda)) // Verifica se a quantidade digitada é um número válido
+                            {
+                                if (produtoVenda.RegistrarVenda(quantidadeVenda))// Verifica se a venda é valida (quantidade disponível no estoque) e registra a venda
+                                {
+                                    decimal totalVenda = produtoVenda.CalcularVenda(quantidadeVenda); 
+                                    int idVendaRegistro = vendasRealizadas.Any() ? vendasRealizadas.Max(v => v.Id) + 1 : 1; // Gerar um ID sequencial para a venda
+                                    vendasRealizadas.Add(new Venda(idVendaRegistro, produtoVenda, quantidadeVenda, totalVenda));
+                                }
+                                
+                            }
+                            else // Quantidade com números inválidos, cancela a operação
+                            {
+                                Console.WriteLine("Quantidade inválida. Operação cancelada.");
+                            }
+
+                        }
+                        else // Produto não encontrado no estoque, exibe mensagem de erro
+                        {
+                            Console.WriteLine("Produto não encontrado no estoque...");
+                        }
+                        Console.WriteLine("\n------------------------------------");
+                        Console.WriteLine("Pressione qualquer tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+
+                    case 3:  // Relatórios (relatório de vendas, relatório de estoque, e fechamento de caixa)
+                        Console.Clear();
+                        Console.WriteLine("--- Relatórios ---");
+                        Console.WriteLine("1. Relatório de Estoque");
+                        Console.WriteLine("2. Relatório de Vendas");
+                        Console.WriteLine("3. Fechamento de Caixa");
+                        Console.Write("\nEscolha: ");
+
+                        if (!int.TryParse(Console.ReadLine(), out int escolhaRelatorio))
+                        {
+                            Console.WriteLine("Opção inválida. Retornando ao menu principal...");
+                            escolhaRelatorio = default; // Sai do switch para retornar ao menu principal
+                        }
+                        else
+                        {
+                            switch (escolhaRelatorio)
+                            {
+                                case 1:
+                                    Console.Clear();
+                                    Console.WriteLine("--- Relatório de Estoque ---\n");
+                                    // Lógica para gerar o relatório de estoque usando "LINQ"
+                                    if (!produtoEstoque.Any())
+                                    {
+                                        Console.WriteLine("O estoque está vazio...");
+                                    }
+                                    else
+                                    {
+                                        produtoEstoque.Where(pe => pe.Quantidade > 0)
+                                        .OrderByDescending(pe => pe.Preco)
+                                        .ToList()
+                                        .ForEach(pe => Console.WriteLine(pe.ExibirDetalhes()));
+                                    }
+                                    break;
+
+                                case 2:
+                                    Console.Clear();
+                                    Console.WriteLine("--- Relatório de Vendas ---\n");
+                                    // Lógica para gerar o relatório de vendas usando "LINQ"
+                                    if (!vendasRealizadas.Any())
+                                    {
+                                        Console.WriteLine("Nenhuma venda realizada ainda...");
+                                    }
+                                    else
+                                    {
+                                        vendasRealizadas.ForEach(vr => Console.WriteLine(vr.ExibirDetalhes()));
+                                    }
+                                    break;
+
+                                case 3:
+                                    Console.Clear();
+                                    Console.WriteLine("--- Fechamento de Caixa ---\n");
+                                    // Lógica para calcular o total do caixa usando "LINQ"
+                                    decimal totalCaixa = vendasRealizadas.Sum(v => v.TotalVenda);
+                                    Console.WriteLine($"Total do Caixa: {totalCaixa:C}");
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Opção inválida. Retornando ao menu principal...");
+                                    break;
+                            }
+                        }
+                        Console.WriteLine("\n------------------------------------");
+                        Console.WriteLine("Pressione qualquer tecla para voltar...");
+                        Console.ReadKey();
+                        break;
+                    case 4: // Sair do sistema
+                        Console.WriteLine("\nSaindo do sistema. Obrigado por usar a Doninha Encantada!");
+                        break;  
+
+                    default: // Opção inválida
+                        Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
+                        break;
+                }
+
+
+            }while (opcoes != 4);
+
+        }
+    }
+}   
---
+++ Project - A Doninha Encantada/Service.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_A_Doninha_Encantada_Classes
+{
+    // --- CLASSE PAI ---
+    public abstract class ProdutoRPG       
+    {
+        public string Id { get; private set; }
+        public string Nome { get; private set; }
+        public decimal Preco { get; private set; }
+
+        public int Quantidade { get; private set; }
+
+        public ProdutoRPG(string id, string nome, decimal preco, int quantidade)
+        {
+            // Validações para garantir que os dados do produto sejam válidos
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                throw new ArgumentException("O nome do produto não pode ser vazio.", nameof(nome));   
+            }
+
+            if(preco < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(preco), "O preço do produto não pode ser negativo.");
+            }
+            
+            if(string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentNullException(nameof(id), "O ID do produto não pode ser vazio.");
+            }
+
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        // Método para atualizar o preço de um produto específico, garantindo que o novo preço seja válido
+        public void AtualizarPreco(decimal novoPreco)
+        {
+            if (novoPreco < 0)
+             throw new ArgumentOutOfRangeException(nameof(novoPreco), "O preço do produto não pode ser negativo.");
+            
+            Preco = novoPreco;
+            Console.WriteLine($"O preço do produto {Nome} foi atualizado para {Preco:C}.");
+        }
+
+        // Método para repor o estoque de um produto, garantindo que a quantidade adicionada seja positiva
+        public void ReporEstoque(int quantidade)
+        {
+            if (quantidade <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(quantidade), "A quantidade a ser reabastecida deve ser maior que zero.");
+            }
+
+            Quantidade += quantidade;
+            Console.WriteLine($"O estoque do produto {Nome} foi reposto. Quantidade atual: {Quantidade}.");
+        }
+
+
+        // Método para calcular o valor total de uma venda
+        public virtual decimal CalcularVenda(int quantidadeVendida)
+        {
+             decimal totalVenda = quantidadeVendida * Preco;
+             return totalVenda;
+        }
+
+        // Método para registrar uma venda, verificando se há estoque suficiente antes de processar a venda e atualizar o estoque
+        // Retorna true se a venda foi registrada com sucesso, ou false se não há estoque suficiente
+        public virtual bool RegistrarVenda(int quantidadeVendida)
+        {
+     
+            if (quantidadeVendida <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(quantidadeVendida), "A quantidade vendida deve ser maior que zero.");
+            }
+
+            if (quantidadeVendida <= Quantidade)
+            {
+                Quantidade -= quantidadeVendida;
+                decimal totalVenda = CalcularVenda(quantidadeVendida);
+                Console.WriteLine($"Venda registrada: {quantidadeVendida} unidades de {Nome} vendidas por R${totalVenda}. Estoque restante: {Quantidade}.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Não há estoque suficiente para vender {quantidadeVendida} unidades de {Nome}. Estoque atual: {Quantidade}.");
+                return false;
+            }
+            
+        }
+
+        // Método para exibir os detalhes de um produto
+        public virtual string ExibirDetalhes()
+        {
+            return $"[{Id}] {Nome} - Preço: {Preco:C} - Estoque: {Quantidade}";
+        }
+    }
+
+    // --- CLASSE DE VENDAS ---
+    public class Venda
+    {
+        public int Id { get; private set; }
+        public ProdutoRPG Produto { get; private set; }
+        public int QuantidadeVendida { get; private set; }
+        public decimal TotalVenda { get; private set; }
+        public Venda(int id, ProdutoRPG produto, int quantidadeVendida, decimal totalVenda)
+        {
+            Id = id;
+            Produto = produto;
+            QuantidadeVendida = quantidadeVendida;
+            TotalVenda = totalVenda;
+        }
+
+        // Método para exibir os detalhes de uma venda
+        public string ExibirDetalhes()
+        {
+            return $"Venda {Id} Item: {Produto.Nome} - Quantidade: {QuantidadeVendida} - Total: {TotalVenda:C}";
+        }
+    }
+
+    // --- CLASSES FILHAS ---
+
+    public class Equipamento : ProdutoRPG
+    {
+        public string TipoEquipamento { get; private set; }
+        public Equipamento(string id, string nome, decimal preco, int quantidade, string tipoEquipamento)
+            : base(id, nome, preco, quantidade)
+        {
+            TipoEquipamento = tipoEquipamento;
+        }
+
+        // Sobrescreve o método ExibirDetalhes para incluir informações específicas do equipamento
+        public override string ExibirDetalhes()
+        {
+            return base.ExibirDetalhes() + $" - Tipo: {TipoEquipamento} (Equipamento)";
+        }
+
+        // Sobrescreve o método CalcularVenda para aplicar uma taxa
+        public override decimal CalcularVenda(int quantidadeVendida)
+        {
+
+            decimal totalVenda = base.CalcularVenda(quantidadeVendida);
+
+            totalVenda *= 1.15m; // Aumenta o preço em 15% para equipamentos
+            return totalVenda;
+        }
+
+        // Sobrescreve o método RegistrarVenda para informar que uma taxa foi aplicada para a compra de equipamentos
+        public override bool RegistrarVenda(int quantidadeVendida)
+        {
+            bool sucesso = base.RegistrarVenda(quantidadeVendida);
+            if (sucesso)
+            {
+                Console.WriteLine($"Frete de 15% aplicado na compra de equipamentos");
+            }
+            return sucesso;
+
+        }
+    }
+
+    public class Consumivel : ProdutoRPG
+    {
+        public string Efeito { get; private set; }
+        public Consumivel(string id, string nome, decimal preco, int quantidade, string efeito)
+            : base(id, nome, preco, quantidade)
+        {
+            Efeito = efeito;
+        }
+
+        // Sobrescreve o método ExibirDetalhes para incluir informações específicas do consumível
+        public override string ExibirDetalhes()
+        {
+            return base.ExibirDetalhes() + $" - Efeito: {Efeito} (Consumível)";
+        }
+
+        // Sobrescreve o método RegistrarVenda informando os Descontos aplicados
+        public override bool RegistrarVenda(int quantidadeVendida)
+        {
+            bool sucesso = base.RegistrarVenda(quantidadeVendida);
+            if (sucesso)
+            {
+                if (quantidadeVendida >= 3 && quantidadeVendida < 20)
+                {
+                    Console.WriteLine($"Desconto aplicado: 20% em compras acima de 3 unidades.");
+                }
+                if (quantidadeVendida >= 20)
+                {
+                    Console.WriteLine($"Desconto aplicado: 35% em compras acima de 20 unidades.");
+                }
+            }
+            return sucesso;
+
+        }
+
+        // Sobrescreve o método CalcularVenda para aplicar um descontos
+        public override decimal CalcularVenda(int quantidadeVendida)
+        {
+            decimal totalVenda = base.CalcularVenda(quantidadeVendida);
+            if (quantidadeVendida >= 3 && quantidadeVendida < 20)
+            {
+                totalVenda *= 0.8m; // Reduz o preço em 20% para consumíveis
+            }
+            if (quantidadeVendida >= 20)
+            {
+                totalVenda *= 0.65m; // Reduz o preço em 35% para compras acima de 20 unidades de consumíveis
+            }
+
+            return totalVenda;
+        }
+    }
+}
